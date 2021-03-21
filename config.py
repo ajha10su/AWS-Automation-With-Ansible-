849b181138edd81312f031cabd6889800db48788
ajha10su <74584504+ajha10su@users.noreply.github.com>
2021-03-21 14:52:49 -0400

    Add files via upload

---
+++ config.py
@@ -0,0 +1,6 @@
+customhost = "scriptingfinal651.cpk67mivo0rf.us-east-1.rds.amazonaws.com"
+customuser = "mysql_admin"
+custompass =  # password of database
+customdb = "employee"
+custombucket = "projectfinal5651"
+customregion = "us-east-1"
